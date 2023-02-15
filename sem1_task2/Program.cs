e5b519269490aef4e1054b987513eeeb6237056a
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-15 17:33:20 +0600

    Задача №2

---
+++ sem1_task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число 1: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2: ");
+int y = int.Parse(Console.ReadLine()!);
+if (x > y) 
+{
+    Console.Write("max число: ");
+    Console.WriteLine(x);
+    Console.Write("min число: ");
+    Console.WriteLine(y);
+    
+
+}
+else 
+{
+    Console.Write("max число: ");
+    Console.WriteLine(y);
+    Console.Write("min число: ");
+    Console.WriteLine(x);
+}
